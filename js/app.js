835530ad523e1c1f6de5d2c285feca5f6dca54aa
student <visa.zaporozie.shengen@gmail.com>
2019-12-04 20:35:21 +0200

    use interpolation

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
 console.log('App was loaded...');
+
+let str1 = {en: 'Hello', ru: 'Привет'};
+let str2 = {en: 'Hello', ru: 'Мир'};
+let str3 = {en: 'Hello', ru: 'Привет'};
+const locale = 'ru';
+// console.log('text 1 : text 2');
+console.log(`${str1[locale]}  ${str2[locale]} ${str3[locale]}`);
